ace0d8df338c1e3b2571df474bdfa37fea85c605
@Coder2100 <mtotsoz@gmail.com>
2019-06-27 01:27:58 +0200

    Wrapping API views

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from snippets import views
 
+from rest_framework.urlpatterns import format_suffix_patterns
+
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+#http://127.0.0.1:8000/snippets/
+#http://127.0.0.1:8000/snippets.api/
+#http://127.0.0.1:8000/snippets.json/
--- snippets/views.py
+++ snippets/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse, JsonResponse
+#from django.http import HttpResponse, JsonResponse
+# removing Json pass while using rest framwewok decorators
+from django.http import HttpResponse
 
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+# imports for Wrapping API views decoratrs used
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+#from snippets.serializers import SnipperSerializer
 
+"""
 @csrf_exempt
 def snippet_list(request):
-    """
-    List all code snippets, or create a new snippet.
-    """
+    
+    #List all code snippets, or create a new snippet.
+    
     if request.method == 'GET':
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
@@ -27,7 +35,7 @@ def snippet_list(request):
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
 
-"""
+
 def snippet_list(request):
     #list all the code snippets or create new
     if request.method == 'GET':
@@ -67,13 +75,11 @@ def snippet_detail(request, pk):
     elif request.method == 'DELETE':
         snippet.delete()
         return HttpResponse(status=204)
-
-        """
 @csrf_exempt
 def snippet_detail(request, pk):
-    """
-    Retrieve, update or delete a code snippet.
-    """
+   
+   # Retrieve, update or delete a code snippet.
+  
     try:
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
@@ -94,3 +100,42 @@ def snippet_detail(request, pk):
     elif request.method == 'DELETE':
         snippet.delete()
         return HttpResponse(status=204)
+
+
+        """
+#implementing Wrapping API views,Requests and Responses
+
+@api_view(['GET', 'POST'])
+def snippet_list(request,format=None):
+    #list all code snippet, or create a new snippet
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['GET', 'PUT','DELETE'])
+def snippet_detail(request, pk, format=None):
+    #Retrieve, update or delete a code snippet
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = SnippetSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serialize.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
