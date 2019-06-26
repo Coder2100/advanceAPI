6155389d321c14fc7336124aad64dbd342b5cbb1
@Coder2100 <mtotsoz@gmail.com>
2019-06-26 23:57:20 +0200

    snippets Models

---
+++ snippets/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+
+from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+"""
+class SnippetSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    linenos = serializers.BooleanField(required=False)
+    code = serializers.CharField(style={'base_template':'textarea.html'})
+    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+    #language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+"""
+
+# more efficient way,  refactoring our serializer using the ModelSerializer
+
+"""
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'style')
+
+    def create(self, validated_data):
+        #create and return snippet instance, given the validated data
+        return Snippet.objects.create(**validated_data)
+    def update(self, instance, validated_data):
+        #update and return snippert instance given the validatd data
+        instance.title = validated.get('title', instance.title)
+        instance.code = validate_data.get('code', instance.code)
+        instance.linenos = validated_data.get('linenos', instance.linenos)
+        instance.language = validated_data.get('language', instance.lanaguage)
+        instance.style = validated_data.get('style', instance.style)
+        instance.save()
+
+        return instance
+
+"""
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+    
